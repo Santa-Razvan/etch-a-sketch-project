f2b0e88b9a4e9bec7ed306ff0f0c8a638916779c
Santa-Razvan <126788093+Santa-Razvan@users.noreplay.github.com>
2023-04-04 17:15:48 +0300

    added 2 buttons that work perfectly fine, solved the bugs, need to see if anything else needs implementation

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ clearButton.textContent = "Clear";
 clearButton.style.cssText = "padding:6px 30px; border:1px solid black; border-radius:6px; background-color:rgb(200,124,50);";
 buttonContainer.appendChild(clearButton);
 
-let numberSquares;
-
 squaresButton.addEventListener('mouseenter', () => {
     squaresButton.style.backgroundColor = 'rgb(180,104,30)';
 });
@@ -31,7 +29,22 @@ clearButton.addEventListener('mouseleave', () => {
     clearButton.style.backgroundColor = 'rgb(200,124,50)';
 });
 
-squaresButton.addEventListener('click', (e) => {
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        const squares = document.createElement('div');
+        squares.style.cssText = "height:10px; width:10px; border:0.1px solid black; background-color: white;";
+        container.style.cssText = "height:200px;width:300px;";
+        squares.addEventListener("mouseenter", (e) => {
+            e.target.style.backgroundColor = "blue";
+        });
+        container.append(squares);
+    }
+}
+
+let numberSquares;
+
+squaresButton.addEventListener('click', () => {
     do {
         numberSquares = prompt("Pick a number of squares between 1-100 to set your drawing space: ");
         if (numberSquares === null) {
@@ -39,24 +52,28 @@ squaresButton.addEventListener('click', (e) => {
         }
     } while (!((numberSquares > 0) && (numberSquares < 101)));
 
+    const allSquares = document.querySelectorAll('.container div');
+    allSquares.forEach(square => container.removeChild(square));
+
     for (let i = 0; i < numberSquares; i++) {
         for (let j = 0; j < numberSquares; j++) {
             const squares = document.createElement('div');
             squares.style.cssText = "height:10px; width:10px; border:0.1px solid black; background-color: white;";
-            container.style.cssText = "height:700px;width:1650px;";
+            container.style.cssText = "border: 1px solid black; border-radius: 1rem; padding: 30px 30px; display: flex; flex-wrap: wrap; align-items: center;justify-content: center;background-color: red;height:700px;width:1650px;";
             squares.addEventListener("mouseenter", (e) => {
                 e.target.style.backgroundColor = "blue";
             });
             container.appendChild(squares);
         }
-
     }
 });
 
+
 clearButton.addEventListener('click', () => {
-    for (let i = 0; i < numberSquares; i++) {
-        for (let j = 0; j < numberSquares; j++) {
-            squares.style.backgroundColor = "white";
+    const allSquares = document.querySelectorAll('.container div');
+    for (let i = 0; i < allSquares.length; i++) {
+        for (let j = 0; j < allSquares.length; j++) {
+            allSquares[i].style.backgroundColor = "white";
         }
     }
 });
